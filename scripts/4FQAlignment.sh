89adf6e4567a37b54af3d6881a009183afd62624
mjzapata <malcolmzapata@gmail.com>
2014-05-19 16:01:04 -0400

    4FQ alignment with added Java code for renaming

--- scripts/4FQAlignment.sh
+++ scripts/4FQAlignment.sh
@@ -3,39 +3,27 @@
 #PBS -N mjzFastAtoMusc
 #PBS -l walltime=0:150:00
 #PBS -l nodes=60
-. /Users/malcolm/Documents/github/WholeGenomeAnalytics/scripts/wgaconfig.conf
+. wgaconfig.conf
 mkdir ${mergedFADir}
-#Find list of sorted FQ Files
+#1.)Find list of sorted FQ Files
+i=0
+while read line
+do
+    echo $line
+    java -classpath ${programsDir} renameFastaHeader $line ${line}_out.fq
+    rm ${line}
+(( i++ ))
+done < <(find ${snpCalledDir}*.fq)
+
+#2.)Find list of sorted RENAMED FQ Files
 i=0
 while read line
 do
     fqFiles[ $i ]="$line"
+    echo $line
 (( i++ ))
 done < <(find ${snpCalledDir}*.fq)
 
-
-
-## CHANGE fasta names inside each file for each fasta file
-# sed "1s/.*/$var/" ERR026015.bam.sorted.bam.fq > test.txt
-while read line
-do
-    fqFileNames[ $i ]="$line"
-(( i++ ))
-done < <(find ${snpCalledDir}*.fq -type f -exec basename {} \;)
-#echo ${fqFileNames[@]}
-
-for i in "${fqFileNames[@]}"
-do
-#   x=${fqFileNames[i]}
-    #echo ${x%%.*}
-    #echo ${fqFileNames[$i]}
-    var1='@'
-    #var2=${fqFileNames[i]}
-    var2=$i
-    var=$var1$var2
-    sed "1s/.*/$var/" ${fqFiles[i]} > ${fqFiles[i]}
-done
-########################################################
 
 
 #run mpileup on sorted bamfiles
