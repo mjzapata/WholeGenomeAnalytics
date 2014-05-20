9050ad938a0230c5a194601643a296db0cea8634
mjzapata <malcolmzapata@gmail.com>
2014-05-20 03:41:48 -0400

    Added Java code to 4FQ Alignment and 5TreeBuild
    seems to be working all the way through

--- scripts/5TreeBuild.sh
+++ scripts/5TreeBuild.sh
@@ -5,55 +5,16 @@
 #PBS -l nodes=60
 #*****bug clear the trimmed file first since it is being added to in a loop
 . /users/mjzapata/scripts/wgaconfig.conf
-mkdir ${mergedFADir}
+
 ##http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
 
 in_File=${mergedFADir}merged.fa
 trimmed_File=${mergedFADir}merged_trimmed.fa
 
-############ make sure same length
-i=0
-while read line
-do
-    rows[$i]="$line"
-    ((i++))
-done < $in_File
-arrayLength=${#rows[@]}
-echo number of elements: $arrayLength
+${javaCall} -jar ${programsDir}uniformSeqLength.jar $in_File $trimmed_File
 
-
-# Break the main text up into rows
-for (( i=0; i < $arrayLength; i++))
-do
-    if [ "${rows[i]:0:1}" != ">" ]
-then
-    counts[i]=${#rows[$i]:0:1}
-fi
-done
-
-# Find the smallest Value
-SMALLEST=${counts[1]}
-for i in ${counts[@]}; do
-if [ $i -lt $SMALLEST ]; then
-    SMALLEST=$i
-fi
-done
-printf "%s\n" "Smallest number is $SMALLEST"
-
-# Print the truncated rows
-for (( i=0; i < $arrayLength; i++))
-do
-    if [ "${rows[i]:0:1}" != ">" ]
-    then
-        echo ${rows[$i]:0:$SMALLEST} >> $trimmed_File
-    else
-        echo ${rows[$i]} >> $trimmed_File
-    fi
-done
-############ make sure same length
-
-#${programsDir}FastTreeMP -fastest -nt -gtr $trimmed_File > ${mergedFADir}tree
-${programsDir}FastTreeMP -fastest -nt -gtr $in_File > ${mergedFADir}tree
+${programsDir}FastTreeMP -fastest -nt -gtr $trimmed_File > ${mergedFADir}tree
+#${programsDir}FastTreeMP -fastest -nt -gtr $in_File > ${mergedFADir}tree
 # Problems,
 #1.) sequences not the same length
 #2.) Duplicate names
